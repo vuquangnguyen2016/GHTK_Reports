2d5cedc877ce1051f4eef1e4aea7a4988d82a906
le quang <lequang@localhost.localdomain>
2019-12-30 23:59:20 +0700

    update

---
+++ ReportQ1/GHTKDataMining/GHTKScriptMining (localhost.localdomain's conflicted copy 2019-12-29).R
@@ -0,0 +1,15 @@
+library(tidyverse)         
+library(readxl)
+library(writexl)
+
+install.packages("googlesheets")
+
+
+# Unix 
+Sys.setlocale("LC_ALL", 'en_US.UTF-8')
+# Windows 
+Sys.setlocale("LC_ALL", "en_US.ISO8859-1")
+
+# Import---------------------------------------------------------------------------------------
+
+rm(list = ls())
---
+++ Source/ok (localhost.localdomain's conflicted copy 2019-12-29).R
@@ -0,0 +1,13 @@
+# https://www.analyticsvidhya.com/blog/2019/05/beginner-guide-tidyverse-most-powerful-collection-r-packages-data-science/
+setwd("~/Dropbox/DataCenter/GHTK/GHTK_Reports/AnalysingVolume")
+rm(list = ls())
+library(readxl)
+library(dplyr)
+
+
+dataRaw <- read_excel("Dataset/hoa_don_cod_20191218171546_5df9fc52-da8c-4e03-8ef4-b0120a0a026e.xlsx", 
+                      sheet = "Hóa đơn COD")
+
+dataRaw  %>%
+  select(`SL ĐH`, Cod) %>%
+  filter(`SL ĐH` >10)  %>% head()
